f8bf77ede69385ebd9f04b9906de49e7e7fc7cf4
lucassant <lucasfdesantana095@gmail.com>
2026-08-15 20:54:29 -0300

    contagem regressiva

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 // console.log('Mensagem teste')
 
-let numero = 2
-let resultado = numero % 2 == 0 ? `O número ${numero} é PAR` : `O número ${numero} é IMPAR`
+// let numero = 2
+// let resultado = numero % 2 == 0 ? `O número ${numero} é PAR` : `O número ${numero} é IMPAR`
 
-console.log(resultado)
+// console.log(resultado)
+
+
+function contagemRegressiva(num){
+    for(let i = num; i > 0; i--){
+        console.log(`- ${i}`)
+    }
+    
+}
+
+contagemRegressiva(10)
