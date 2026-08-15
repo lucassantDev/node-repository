00803c9e857cf6dda79db371f42a3fb8fd0a4a4a
lucassant <lucasfdesantana095@gmail.com>
2026-08-15 20:46:50 -0300

    operadores ternarios

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('Mensagem teste')
+// console.log('Mensagem teste')
+
+let numero = 2
+let resultado = numero % 2 == 0 ? `O número ${numero} é PAR` : `O número ${numero} é IMPAR`
+
+console.log(resultado)
