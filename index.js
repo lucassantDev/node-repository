a2b7ec1e0a3be5b88dbe215cf4a23d4dfb4f9fbf
lucassant <lucasfdesantana095@gmail.com>
2026-08-16 00:34:30 -0300

    funcao de contar vogais com lista e metodo include

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-// contando vogais utilizando a logica de listas e operadores ternarios
+// contando vogais utilizando a logica de listas e método includes
 function contarVogais(char){
     let palavra = char.toLowerCase()
     vogais = []
     consoantes = []
     for(caracter of palavra){
         console.log(caracter)
-        caracter === 'a' ? vogais.push(caracter) : 
-        caracter === 'e' ? vogais.push(caracter) :
-        caracter === 'i' ? vogais.push(caracter) :
-        caracter === 'o' ? vogais.push(caracter) :
-        caracter === 'u' ? vogais.push(caracter) :
-        consoantes.unshift(caracter)
+        if('aeiou'.includes(caracter)){
+            vogais.push(caracter)
+        }else{
+            consoantes.push(caracter)
+        }
 
     } 
     
     console.log(`Há ${vogais.length} vogais: ${vogais}`)
-    console.log(consoantes)
+    console.log(`Há ${consoantes.length} consoantes: ${consoantes}`)
     }
     
 contarVogais('Lucas')
