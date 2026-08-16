c922cf6b51d46bf6bb194e1a1885b67c35428687
lucassant <lucasfdesantana095@gmail.com>
2026-08-16 00:06:09 -0300

    feat: funcao para contar vogais com operadores ternarios e listas

--- index.js
+++ index.js
@@ -1,39 +1,21 @@
-// console.log('Mensagem teste')
+// contando vogais utilizando a logica de listas e operadores ternarios
+function contarVogais(char){
+    let palavra = char.toLowerCase()
+    vogais = []
+    consoantes = []
+    for(caracter of palavra){
+        console.log(caracter)
+        caracter === 'a' ? vogais.push(caracter) : 
+        caracter === 'e' ? vogais.push(caracter) :
+        caracter === 'i' ? vogais.push(caracter) :
+        caracter === 'o' ? vogais.push(caracter) :
+        caracter === 'u' ? vogais.push(caracter) :
+        consoantes.unshift(caracter)
 
-// let numero = 2
-// let resultado = numero % 2 == 0 ? `O número ${numero} é PAR` : `O número ${numero} é IMPAR`
-
-// console.log(resultado)
-
-
-// function contagemRegressiva(num){
-//     for(let i = num; i > 0; i--){
-//         console.log(`- ${i}`)
-//     }
+    } 
     
-// }
-
-// contagemRegressiva(10)
-
-
-// 
-// let total = 0
-
-// for(const num of numeros){
-//     total+= num
-// }
-
-// console.log(total)
-
-let numeros = [1, 2, 5, 6]
-function somarValoresArray(lista){
-    let valorTotal = 0
-    for(const num of lista){
-        valorTotal += num 
-        
+    console.log(`Há ${vogais.length} vogais: ${vogais}`)
+    console.log(consoantes)
     }
-
-    console.log(valorTotal)
-}
-
-somarValoresArray(numeros)
+    
+contarVogais('Lucas')
