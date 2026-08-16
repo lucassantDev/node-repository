d5926c8f622bcfb51bc38033d4012554277299b8
lucassant <lucasfdesantana095@gmail.com>
2026-08-15 21:23:35 -0300

    for of somando numeros de array

--- index.js
+++ index.js
@@ -6,11 +6,34 @@
 // console.log(resultado)
 
 
-function contagemRegressiva(num){
-    for(let i = num; i > 0; i--){
-        console.log(`- ${i}`)
-    }
+// function contagemRegressiva(num){
+//     for(let i = num; i > 0; i--){
+//         console.log(`- ${i}`)
+//     }
     
+// }
+
+// contagemRegressiva(10)
+
+
+// 
+// let total = 0
+
+// for(const num of numeros){
+//     total+= num
+// }
+
+// console.log(total)
+
+let numeros = [1, 2, 5, 6]
+function somarValoresArray(lista){
+    let valorTotal = 0
+    for(const num of lista){
+        valorTotal += num 
+        
+    }
+
+    console.log(valorTotal)
 }
 
-contagemRegressiva(10)
+somarValoresArray(numeros)
