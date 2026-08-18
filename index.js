95291ad0121d39f6b167b3f35c936c7f5e673c87
lucassant <lucasfdesantana095@gmail.com>
2026-08-18 10:50:48 -0300

    feat: adicionando metodo splice para remover livro de array

--- index.js
+++ index.js
@@ -31,24 +31,36 @@ function alugarLivro(arr) {
     let prompt = require('prompt-sync')();
     let livroDesejado = Number(prompt('Qual Livro deseja alugar? '))
     let data = new Date()
+
+    let aluguel = (arr[livroDesejado])
+    let livroAlugado = arr.indexOf(aluguel)
+    if (livroAlugado !== -1) {
+        arr.splice(livroAlugado, 1)
+    }
+    
     console.log('-------------------------------------------------')
     
-
-    console.log(`O livro ${arr[livroDesejado]} será alugado pelos proximos 30 dias! Retorne no dia ${data.getDate()}/0${data.getMonth()}/${data.getFullYear()}`)
+    console.log('Livros Disponíveis agora:')
+    console.log('--------------------------')
+    for (livro of arr) {
+        console.log(livro)
+    }
+    console.log('--------------------------')
+    console.log(`O livro ${aluguel} será alugado pelos proximos 30 dias! Retorne no dia ${data.getDate()}/0${data.getMonth()}/${data.getFullYear()}`)
 }
 
 resposta = 0
 console.log('Iniciando Programa')
 while (resposta != 4) {
     const prompt = require('prompt-sync')();
-    console.log('----------------------\n1- Livros Disponiveis\n2- Cadastrar Novo Livro\n3- Alugar Livro\n----------------------\n')
+    console.log('----------------------\n1- Livros Disponiveis\n2- Cadastrar Novo Livro\n3- Alugar Livro\n4- Sair\n----------------------')
     let resposta = Number(prompt('Qual sua escolha? '));
     if (resposta === 1) {
-        livrosDisponiveis()
+        livrosDisponiveis() 
     } else if (resposta === 2) {
         cadastrarNovoLivro()
     } else if (resposta === 3) {
-        alugarLivro()
+        alugarLivro(livros)
     } else if (resposta === 4) {
         console.log('Finalizando o programa...')
         break
