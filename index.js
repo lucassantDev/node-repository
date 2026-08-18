9f7c30d7a71038e3fde6eeffe18e413a73b704d6
lucassant <lucasfdesantana095@gmail.com>
2026-08-17 23:40:30 -0300

    chore: gitignore

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
-// contando vogais utilizando a logica de listas e método includes
-function contarVogais(char){
-    let palavra = char.toLowerCase()
-    vogais = []
-    consoantes = []
-    for(caracter of palavra){
-        console.log(caracter)
-        if('aeiou'.includes(caracter)){
-            vogais.push(caracter)
-        }else{
-            consoantes.push(caracter)
-        }
 
-    } 
-    
-    console.log(`Há ${vogais.length} vogais: ${vogais}`)
-    console.log(`Há ${consoantes.length} consoantes: ${consoantes}`)
-    }
-    
-contarVogais('Lucas')
+const letras = ['a', 'e', 'r', 'b']
+
+console.log(letras)
+
+letras.sort()
+
+letras.unshift('js')
+letras.push('g')
+console.log(letras)
+
+const prompt = require('prompt-sync')();
+const user = prompt('Letra: ');
+
+console.log(letras.includes(user) ? `A letra ${user.toUpperCase()} está na lista` : `A letra ${user.toUpperCase()} não está na lista` )
